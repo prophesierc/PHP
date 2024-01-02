99a7971f9605fc248f8451cdc0bd8471c6ec5f97
ProphesierC <bknowles98@gmail.com>
2024-01-02 06:23:12 +0000

    1/1 update

---
+++ PHP/1_1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Template</title>
+</head>
+<body>
+    <?php
+        class User
+			{
+				public function __construct(private string $name, private string $email)
+				{
+					
+				}
+			}
+    ?>
+</body>
+</html>
